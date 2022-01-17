3fea622bdebbff2e67ffe722f105d6320de662b1
Shivam Tiwari <shivamtiwari8417@gmail.com>
2022-01-17 17:52:58 +0530

    Initial commit

---
+++ src/main/java/com/example/JWT/controller/ApiController.java
@@ -0,0 +1,13 @@
+package com.example.JWT.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello!";
+    }
+}
