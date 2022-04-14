ea718b039b9be893b018e997befdee6e69617ed9
shivamtiwari841797 <44071737+shivamtiwari841797@users.noreply.github.com>
2022-04-14 06:50:44 +0530

    adding logs to test ci/cd trigger

--- src/main/java/com/example/JWT/JwtApplication.java
+++ src/main/java/com/example/JWT/JwtApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JwtApplication {
 
 	public static void main(String[] args) {
+		System.out.println("testing ci/cd");
 		SpringApplication.run(com.example.JWT.JwtApplication.class, args);
 	}
 
